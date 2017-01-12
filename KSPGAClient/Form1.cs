a0794a601f24a1953b7074ab2b1b0f53154c399b
Kamaia <krystalamaia@gmail.com>
2017-01-11 21:56:13 -0800

    Added Things To Things.

--- KSPGAClient/Form1.cs
+++ KSPGAClient/Form1.cs
@@ -20,7 +20,19 @@ namespace KSPGAClient {
 			InitializeComponent( );
 		}
 
-		
+		private void updateConnStatus(object sender, ConnectionStatusUpdateEventArgs e ) {
+			switch ( e.Status ) {
+				case CONNECTIONSTATUS.CONNECTED:
+					ts_TelnetStatus.ForeColor = Color.Green;
+					ts_TelnetStatus.Text = "Telnet: Connected!";
+					break;
+				case CONNECTIONSTATUS.DISCONNECTED:
+				
+				case CONNECTIONSTATUS.NULL:
+					ts_TelnetStatus.ForeColor = Color.Red;
+					ts_TelnetStatus.Text = "Telnet: Disconnected!";
+					break;	
+				}
 
 		private void toolStripStatusLabel1_Click( object sender, EventArgs e ) {
 
